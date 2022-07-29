1fe18f94d6c45e9907e9028e9f220a9bf9c37d38
[deposada74] <[deposada74@misena.edu.co]>
2022-07-29 14:59:11 -0500

    objects java script

---
+++ js/calculadora.js
@@ -0,0 +1,20 @@
+
+let sum;
+let ciete = 7;
+let cinco = 5;
+sum = ciete + cinco;
+console.log(sum);
+
+//sum = sum + 1;
+//sum++;
+sum = sum + 3;
+
+let _sum = 10;
+
+let vec = [10, 99, 24, 1, 100, 2500, "juan", 19800830, 20011022];
+
+let object = {"Ciudad":"Medellín",
+    "Nombre":"Heidy",
+    "Direcion":"Cll 35 sur",
+    "Telefono":"132564"
+};
