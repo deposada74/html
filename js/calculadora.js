64f8f466988351486195b59391baf48db61c0ff6
[deposada74] <[deposada74@misena.edu.co]>
2022-07-29 17:45:07 -0500

    suma en js

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,20 +1,88 @@
+let one = document.getElementById('b1');
+let two = document.getElementById('b2');
+let trhee = document.getElementById('b3');
 
-let sum;
-let ciete = 7;
-let cinco = 5;
-sum = ciete + cinco;
-console.log(sum);
+let four = document.getElementById('b4');
+let five = document.getElementById('b5');
+let six = document.getElementById('b6');
 
-//sum = sum + 1;
-//sum++;
-sum = sum + 3;
+let seven = document.getElementById('b7');
+let eight = document.getElementById('b8');
+let nine = document.getElementById('b9');
 
-let _sum = 10;
+let minus = document.getElementById('menos');
+let zero = document.getElementById('b0');
+let pluz = document.getElementById('mas');
 
-let vec = [10, 99, 24, 1, 100, 2500, "juan", 19800830, 20011022];
+let total = document.getElementById('b');
 
-let object = {"Ciudad":"Medellín",
-    "Nombre":"Heidy",
-    "Direcion":"Cll 35 sur",
-    "Telefono":"132564"
-};
+one.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value+1;
+ 
+});
+
+two.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 2;
+ 
+});
+trhee.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 3;
+ 
+});
+four.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 4;
+ 
+});
+five.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 5;
+ 
+});
+six.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 6;
+ 
+});
+seven.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 7;
+ 
+});
+eight.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 8;
+ 
+});
+nine.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    screen.value = screen.value + 9;
+ 
+});
+zero.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    if(screen.value != 0 ){
+        screen.value = screen.value += 0;
+    }
+});
+
+pluz.addEventListener('click', function(){
+    let screen = document.getElementById('screen');
+    let hide = document.getElementById('hide');
+    hide.value = screen.value;
+    screen.value = ' '; 
+});
+
+total.addEventListener('click', function(){
+    let hide = document.getElementById('hide');
+    let screen = document.getElementById('screen');
+    
+    let suma = parseInt(hide.value) + parseInt(screen.value);
+    
+    screen.value = suma;
+    console.log(suma);
+
+});
---
+++ js/home.js
@@ -0,0 +1,64 @@
+/*
+let one = document.getElementById('b1');
+let two = document.getElementById('b2');
+let trhee = document.getElementById('b3');
+let four = document.getElementById('b4');
+let five = document.getElementById('b5');
+let six = document.getElementById('b6');
+let seven = document.getElementById('b7');
+let eight = document.getElementById('b8');
+let nine = document.getElementById('b9');
+let minus = document.getElementById('menos');
+let zero = document.getElementById('b0');
+let pluz = document.getElementById('mas');
+
+one.addEventListener('click', function(){document.getElementById('screen').innerHTML = '1';});
+
+function one(){document.getElementById('screen')};
+function two(){document.getElementById('screen');console.log = ('2 ');};
+function trhee(){document.getElementById('screen');console.log = ('3 ');};
+
+function four(){document.getElementById('screen').innerHTML = '4 ';};
+function five(){document.getElementById('screen').innerHTML = '5 ';};
+function six(){document.getElementById('screen').innerHTML = '6 ';};
+
+function seven(){document.getElementById('screen').innerHTML = '7 ';};
+function eight(){document.getElementById('screen').innerHTML = '8 ';};
+function nine(){document.getElementById('screen').innerHTML = '9 ';};
+
+function minus(){document.getElementById('screen').innerHTML = '- ';};
+function zero(){document.getElementById('screen').innerHTML = '0 ';};
+function plus(){document.getElementById('screen').innerHTML = '+ ';};
+
+
+
+
+let sum;
+let ciete = 7;
+let cinco = 5;
+sum = ciete + cinco;
+//console.log(sum);
+
+//sum = sum + 1;
+//sum++;
+sum = sum + 3;
+
+let _sum = 10;
+
+let vector = [10, 99, 24, 1, 100, 2500, "juan", 19800830, 20011022];
+
+let object = {"Ciudad":"Medellín",
+"Nombre":"Heidy",
+"Direcion":"Cll 35 sur",
+"Telefono":"132564"
+};
+
+if (vector[2] > object.Telefono)
+{console.log('El valor si es mayor que el objecto', vector[2]);
+}else{
+    console.log('El valor no es mayor que el objecto', object.Telefono);
+};
+for (let indice = 0; indice < 89; indice += 1){
+    console.log(vector[indice]);
+};
+*/
